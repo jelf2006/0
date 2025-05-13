af8654a4e2500348d849e7ef35ced814a316e145
jelf2006 <160517752+jelf2006@users.noreply.github.com>
2025-05-13 19:31:03 +0000

    primeiro capítulos concluido

---
+++ capitulo-1/main0.js
@@ -0,0 +1,9 @@
+let idade = 18
+
+if (idade == 18) {
+  console.log("pode dirigir")
+} else if (idade == 17) {
+  console.log("ano q vem pode dirigir")
+} else {
+  console.log("ainda n pode dirigir")
+}
---
+++ capitulo-1/main2.js
@@ -0,0 +1,7 @@
+let n = 0;
+
+while (n <= 10) {
+  console.log(n);
+  n++;
+}
+console.log("while");
