bca499025b9b875486fc91143d6a58e7932ceab7
jelf2006 <160517752+jelf2006@users.noreply.github.com>
2025-05-28 22:07:57 +0000

    cabei o cap 4

--- capitulo-4/main3.js
+++ capitulo-4/main3.js
@@ -1,3 +1,16 @@
-document.getElementById("myButtom").addEventListener("click", function() {
-    document.getElementById("banana")[0].style.color = "red";
+let x = document.getElementById("myButtom")
+let y = document.getElementById("button2")
+let z = document.getElementById("button3")
+
+
+x.addEventListener("click", function() {
+    document.getElementById("potato").style.color = "red";
+})
+
+y.addEventListener("click", function() {
+    document.getElementById("potato").style.color = "green";
+})
+
+z.addEventListener("click", function() {
+    document.getElementById("potato").style.color = "blue";
 })
